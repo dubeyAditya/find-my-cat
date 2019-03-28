6f793ebce9ee6f9dcc0db1a0e593cea6cf322a49
dubeyAditya <sinister.dubey@gmail.com>
2019-03-28 23:03:19 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <ImageGallary msg="Which is my cat"/>
+  </div>
+</template>
+
+<script>
+import ImageGallary from "./components/ImageGallary.vue";
+
+export default {
+  name: "app",
+  components: {
+    ImageGallary
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ImageGallary.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="appContainer"></div>
+</template>
+
+<script>
+export default {
+  name: "ImageGallary",
+  props: {
+    msg: String
+  }
+};
+</script>
+<style scoped>
+</style>
