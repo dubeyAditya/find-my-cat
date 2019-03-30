df75685589cc10d45f8f240cc5008e648818f269
dubeyAditya <sinister.dubey@gmail.com>
2019-03-31 02:41:51 +0530

    removed code for router

--- server/server.js
+++ server/server.js
@@ -5,6 +5,5 @@ var imageList = require('./imageList');
 var serveStatic = require('serve-static');
 var app = express();
 app.use(serveStatic(path.join(__dirname, "/dist")));
-app.get('/getImages', imageList);
 var port = process.env.PORT || 5000;
 app.listen(port);
