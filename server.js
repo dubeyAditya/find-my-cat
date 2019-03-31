18d931cf933781e3ca0a0c46e659fc26517d790a
dubeyAditya <sinister.dubey@gmail.com>
2019-03-31 14:37:38 +0530

    removed server folder

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 
 var express = require('express');
 var path = require('path');
-var imageList = require('./imageList');
 var serveStatic = require('serve-static');
 var app = express();
 app.use(serveStatic(path.join(__dirname, "/dist")));
 var port = process.env.PORT || 5000;
-app.listen(port);
+app.listen(port, (err) => { console.log("Listening at", port) });
--- server/imageList.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs');
-module.exports = function imageList(req, res) {
-    var images = fs.readSync('../src/assets/img');
-    console.log(images);
-    res.send([]);
-}
