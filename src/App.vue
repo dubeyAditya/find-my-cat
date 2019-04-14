c3534efce6b3f0b33f8cfc8d2f873a5b3b5f2f85
dubeyAditya <sinister.dubey@gmail.com>
2019-04-14 14:20:10 +0530

    added clicks and cat selection

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,16 @@ export default {
 
 <style>
 #app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin: 20px 200px 0px 200px;
+  margin: 15px 0 0 0;
+  width: 99vw;
+  height: 95vh;
 }
 </style>
--- src/components/ImageGallary.vue
+++ src/components/ImageGallary.vue
@@ -4,32 +4,29 @@
       <el-col :span="24">
         <el-card>
           <div slot="header" class="clearfix">
-            <span class="font-large">{{ msg }}</span>
+            <span class="font-large" v-text="getMessage"/>
           </div>
-          <el-row :gutter="20">
-            <div v-if="isCompleted">Hurry!!!</div>
-            <el-col v-else :span="12" :offset="6">
+          <el-row type="flex" justify="center">
+            <el-col v-if="isCompleted" :span="24">
+              <img height="100%" width="100%" :key="1" :src="getMostClicked()">
+            </el-col>
+            <el-col v-else :span="22">
               <el-card shadow="hover">
                 <div class="flex-container">
-                  <el-row>
-                    <transition name="slide" mode="out-in">
-                      <img
-                        height="100%"
-                        width="100%"
-                        class="clickable"
-                        :key="getImageName"
-                        :src="getImagePath"
-                        @click="nextImage"
-                      >
-                    </transition>
+                  <el-row style="height:220px">
+                    <div>
+                      <transition name="slide" mode="out-in">
+                        <img width="100%" class="clickable" :key="getImageName" :src="getImagePath">
+                      </transition>
+                    </div>
                   </el-row>
-                  <el-row type="flex" justify="space-between">
+                  <el-row class="top-padding" type="flex" justify="space-around">
                     <div>
                       <el-button
                         icon="el-icon-error"
                         type="danger"
                         round
-                        @click="nextImage(true)"
+                        @click="nextImage(false)"
                       >No! It's Not.</el-button>
                     </div>
                     <div>
@@ -37,7 +34,7 @@
                         type="success"
                         icon="el-icon-success"
                         round
-                        @click="nextImage(false)"
+                        @click="nextImage(true)"
                       >Yes! It is.</el-button>
                     </div>
                   </el-row>
@@ -50,7 +47,6 @@
     </el-row>
   </div>
 </template>
-
 <script>
 export default {
   name: "ImageGallary",
@@ -59,8 +55,10 @@ export default {
   },
   data() {
     return {
-      imageList: new Set(),
-      currentImage: { name: "1.jpg", clicks: 0 },
+      imageList: [],
+      traversList: [],
+      currentImage: null,
+      previousImage: null,
       base_url: process.env.BASE_URL
     };
   },
@@ -72,28 +70,65 @@ export default {
       return this.currentImage.name;
     },
     isCompleted() {
-      return this.imageList.size === 12;
+      return this.traversList.length === this.imageList.length;
+    },
+    getMessage() {
+      return this.hasNoSelection() && this.isCompleted
+        ? "Seems You are a Robot !"
+        : this.isCompleted
+        ? "Awesome ! Your Most Likely Cat is"
+        : this.msg;
     }
   },
   created() {
     //this.$http.get("/getImages").then(response => (this.imageList = imageList));
+    this.loadImages();
+    this.currentImage = this.getNewImage();
   },
+  mounted() {},
   methods: {
-    getNewImage() {
-      let name = Math.floor(Math.random() * (12 - 1)) + 1;
-      const image = {
+    getImageObject(name) {
+      return {
         name: `${name}.jpg`,
         clicks: 0
       };
-      const existing = [...this.imageList].find(image => image.name === name);
-      if (!existing) {
-        this.imageList.add(image);
-      }
+    },
+    getNewImage() {
+      let name = this.getRandomName();
+      const image = this.imageList.find(image => image.name === name);
+      this.traversList = Array.from(new Set([...this.traversList, image]));
       return image;
     },
+    getRandomName() {
+      let randomIndex = Math.floor(Math.random() * this.imageList.length) + 1;
+      return `${randomIndex}.jpg`;
+    },
     nextImage(ans) {
-      ans ? ++this.currentImage.clicks : "";
+      ans && ++this.currentImage.clicks;
       this.currentImage = this.getNewImage();
+    },
+    hasNoSelection() {
+      return this.traversList.every(image => image.clicks === 0);
+    },
+    loadImages() {
+      let imagelist = Array(13).fill(0);
+      for (let index = 1; index < imagelist.length; index++) {
+        let imageItem = this.getImageObject(index);
+        this.imageList.push(imageItem);
+      }
+      console.log(this.imageList);
+    },
+    getMostClicked() {
+      let maxValue = this.traversList[0];
+      for (let index = 0; index < this.traversList.length; index++) {
+        const currentImage = this.traversList[index];
+        if (maxValue.clicks < currentImage.clicks) {
+          maxValue = currentImage;
+        }
+      }
+      return this.hasNoSelection()
+        ? `./img/robot.jpg`
+        : `./img/${maxValue.name}`;
     }
   }
 };
@@ -117,7 +152,7 @@ export default {
 }
 .slide-enter-active,
 .slide-leave-active {
-  transition: all 300ms ease-out;
+  transition: all 100ms ease-out;
 }
 .font-large {
   font-size: 20px;
@@ -126,7 +161,9 @@ export default {
 .flex-container {
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
-  height: 320px;
+  justify-content: space-around;
+}
+.appContainer {
+  width: 600px;
 }
 </style>
